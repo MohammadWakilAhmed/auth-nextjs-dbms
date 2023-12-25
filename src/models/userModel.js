ac78617322ee9efeb25a8b97bd751713b40db394
MohammadWakilAhmed <wakilahmed.csecu@gmail.com>
2023-12-25 14:24:15 +0600

    Initial commit

---
+++ src/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email address"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordExpiry: Date,
+  verifyTokenExpiry: Date,
+});
+
+const User = mongoose.model.users || mongoose.model("users", userSchema);
+
+export default User;
